9bb1bb3decff27b63a2713bff167b31c48556d0d
matthewarthur <matthewarthur@users.noreply.github.com>
2019-02-13 11:43:50 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=16Q4zKXpzc-9dwn-427b7qyNsQh7cL8P3'
+export_file_name = 'export_flowers.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['tulip', 'zinnia']
 path = Path(__file__).parent
 
 app = Starlette()
